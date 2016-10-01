3f587587f99c5b80a5c7bee964bdab8916fcc906
Eric <eric.ebbesen@gmail.com>
2016-10-01 03:39:29 +0000

    Han quotes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-    render html: "This is toy_app"
+    render html: "It's the ship that made the Kessel Run in less than twelve parsecs. I've outrun Imperial starships. Not the local bulk cruisers mind you, I'm talking about the big Corellian ships now. She's fast enough for you old man. What's the cargo?"
   end
 end

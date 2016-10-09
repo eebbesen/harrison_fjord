2865bc61a1b3ead96334ce4d6d0b30d24a88644a
Eric Ebbesen <eebbesen.git@gmail.com>
2016-10-09 08:48:40 -0500

    Making test more inclusive

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -6,7 +6,7 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
 
     assert_response :success
     assert_select('img').each do |i|
-      assert_equal 'https://fakeimages.com/pic1.jpg', i.attributes['src'].value
+      assert_match /https:\/\/fakeimages\.com\/pic.\.jpg/, i.attributes['src'].value
     end
     assert_select('.quote').each do |q|
       assert !q.text.empty?

13c41c94ca2e00b6e093580fd6cf7c5faea0dd43
Eric Ebbesen <eebbesen.git@gmail.com>
2017-03-13 12:51:11 -0500

    Retooling translation script

--- lib/bing_translate.rb
+++ lib/bing_translate.rb
@@ -1,20 +1,28 @@
 # Getting frequent BingTranslator::Exception ((s:Client) TranslateApiException: Cannot find an active Azure Market Place Translator Subscription associated with the request credentials. : ID=0824.V2_Soap.Translate.51F893D7)
 # so not calling per request
 
-qq = StaticPagesController::QUOTES.map { |q| q }
+t = BingTranslator.new(ENV['BING_CLIENT'], ENV['BING_SECRET'], false, ENV['BING_KEY'])
+qq = StaticPagesController::DANSKE_CITERE.map { |q| q }
 res = {}
+errors = 0
+start = Time.now
 
 while qq.size > 0
   sleep 7
   q = qq.last
   begin
-    t = BingTranslator.new(ENV['BING_CLIENT'], ENV['BING_SECRET'], false, ENV['BING_KEY'])
-    res[q] = t.translate(q, from: 'en', to: 'da')
+    k = q[0]
+    res[k] = t.translate(k, from: 'en', to: 'sv')
+    puts "translated: #{k}"
   rescue => ex
-    puts "#(q}: #{ex.message}"
-    sleep 5
+    puts "#{k}: #{ex.message}"
+    errors += 1
+    sleep 7
     retry
   end
   qq.pop
   sleep 7
 end
+
+puts "Translated #{res.size} values with #{errors.size} errors in #{Time.now - start} seconds."
+

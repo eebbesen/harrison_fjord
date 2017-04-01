617b7793efa7d0b03f8e0b411cd5d6067bc747bd
J2EEbbesen <eebbesen@users.noreply.github.com>
2017-04-01 16:24:44 -0500

    Old Bing Azure API shut down -- using new Bing Cognative API (#36)
    * Old Bing Azure API shut down -- using new Bing Cognative API
    
    * Update static_pages_controller.rb

--- Gemfile
+++ Gemfile
@@ -24,7 +24,7 @@ gem 'jbuilder', '~> 2.5'
 # gem 'redis', '~> 3.0'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
-gem 'bing-search'
+gem 'cognitivebing'
 gem 'bing_translator'
 gem 'bootstrap-sass'
 gem 'newrelic_rpm'
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,19 +2,19 @@ ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
-# BingSearch stub
-require 'bing-search'
-BingResult = Struct.new(:media_url, :thumbnail)
+# CognitiveBing stub
+require 'cognitivebing'
+BingResult = Struct.new(:contentUrl, :thumbnailUrl)
 
-module BingSearch
-  def self.image(_text, offset = 0)
+class CognitiveBing
+  def search(search_term, type)
     puts 'USING FAKE BING'
     thumb1 = BingResult.new('https://fakebing.com/thumb1.jpg')
     pic1 = BingResult.new('https://fakeimages.com/pic1.jpg', thumb1)
     thumb2 = BingResult.new('https://fakebing.com/thumb2.jpg')
     pic2 = BingResult.new('https://fakeimages.com/pic2.jpg', thumb2)
 
-    [pic1, pic2]
+    { value: [pic1, pic2] }
   end
 end
 

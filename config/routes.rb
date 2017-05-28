9b6b3453697ca7e116ed125300492d78828a8f3c
J2EEbbesen <eebbesen@users.noreply.github.com>
2017-05-28 10:09:16 -0500

    routes to select language initially (#48)

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   get 'static_pages/han'
   get 'thumbnails/show'
 
+  get 'se', to: 'static_pages#han', language: 'svenska'
+  get 'dk', to: 'static_pages#han', language: 'dansk'
+
   root 'static_pages#han'
 end

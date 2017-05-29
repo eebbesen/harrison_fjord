d4b4967cdab25eb321a63d4121ee2bd8d960e20f
Eric Ebbesen <eebbesen@eebbesen-mbp.local>
2017-05-29 08:37:58 -0500

    Savepoint

--- app/assets/javascripts/hf.js
+++ app/assets/javascripts/hf.js
@@ -42,25 +42,4 @@ function changeLang(fromLang, toLang){
 
 function selectImage(element){
   element.classList.toggle('selected');
-  manipulateDeletionArray(element.id);
-}
-
-function manipulateDeletionArray(id){
-  element = document.getElementById('markedForDelete')
-
-  arr = null;
-  if (element.value.length > 0) {
-    arr = element.value.split(',');
-  } else {
-    arr = [];
-  }
-
-  index = arr.indexOf(id);
-  if (index > -1){
-    arr.splice(index, 1);
-  } else {
-    arr.push(id);
-  }
-
-  element.value = arr.join(',');
 }

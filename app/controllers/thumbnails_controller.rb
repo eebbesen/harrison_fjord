4c40b8f2aa5d563371b1004395584f7a5b8bede2
Eric Ebbesen <eebbesen@eebbesen-mbp.local>
2017-05-29 17:11:06 -0500

    Now deleting, but need to reset afterwards

--- app/controllers/thumbnails_controller.rb
+++ app/controllers/thumbnails_controller.rb
@@ -7,6 +7,5 @@ class ThumbnailsController < ApplicationController
     to_delete = params['markedForDelete']
     to_delete.split(',').map { |l| Link.destroy l unless l.to_i < 0}
     @results = StaticPagesController.new.send(:pictures)
-    redirect_to t_url
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   get 'static_pages/han'
   get 'thumbnails/show'
   get 't', to: 'thumbnails#show'
+  delete 'thumbnails/destroy'
+
 
   get 'se', to: 'static_pages#han', language: 'svenska'
   get 'dk', to: 'static_pages#han', language: 'dansk'

d5ac2ddc10a57b038f739ff8800b662d752a19df
Eric Ebbesen <eebbesen@eebbesen-mbp.local>
2017-05-29 15:41:20 -0500

    Revert "Savepoint with delete working, but not sure if action will be submitted on click..."
    This reverts commit 2d57ec041d454d49a77fc36c676654395d146703.

--- app/controllers/thumbnails_controller.rb
+++ app/controllers/thumbnails_controller.rb
@@ -2,16 +2,4 @@ class ThumbnailsController < ApplicationController
   def show
     @results ||= StaticPagesController.new.send(:pictures)
   end
-
-  def destroy
-    byebug
-  end
-
-  def get
-    byebug
-  end
-
-  def post
-    byebug
-  end
 end

777c5c9343cd3b4bf5658441ee40d6c9ef873c90
Eric Ebbesen <eebbesen@eebbesen-mbp.local>
2017-05-29 17:40:42 -0500

    Redirect back to thumbnails

--- app/controllers/thumbnails_controller.rb
+++ app/controllers/thumbnails_controller.rb
@@ -7,5 +7,6 @@ class ThumbnailsController < ApplicationController
     to_delete = params['markedForDelete']
     to_delete.split(',').map { |l| Link.destroy l unless l.to_i < 0}
     @results = StaticPagesController.new.send(:pictures)
+    redirect_to t_url
   end
 end

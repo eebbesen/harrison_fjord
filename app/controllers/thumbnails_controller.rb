2d57ec041d454d49a77fc36c676654395d146703
Eric Ebbesen <eebbesen@eebbesen-mbp.local>
2017-05-29 08:38:27 -0500

    Savepoint with delete working, but not sure if action will be submitted on click...

--- app/controllers/thumbnails_controller.rb
+++ app/controllers/thumbnails_controller.rb
@@ -2,4 +2,16 @@ class ThumbnailsController < ApplicationController
   def show
     @results ||= StaticPagesController.new.send(:pictures)
   end
+
+  def destroy
+    byebug
+  end
+
+  def get
+    byebug
+  end
+
+  def post
+    byebug
+  end
 end

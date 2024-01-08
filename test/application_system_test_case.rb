6622a64f13882107686a634196949021b5caa0b3
eebbesen <eebbesen.git@gmail.com>
2024-01-08 14:50:46 -0600

    linter remediation

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -2,6 +2,7 @@
 
 require 'test_helper'
 
+# app system test case
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
 end
